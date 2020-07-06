84d75407cc0ea89c70dfd0e799980211db351afd
Igor Makarov <igormaka@gmail.com>
2020-07-06 18:04:36 +0300

    update changelog

--- CHANGELOG.rb
+++ CHANGELOG.rb
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.0.3
+
+* Remove some code that was limiting functionality - now packages that vend differently-named libraries are supported
+
 ## 0.0.2
 
 * Support local packages
